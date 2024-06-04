ccf175b7b1e294e50c47e4127a1b018a0c254f77
Janet Core <arthur@janetcore.com>
2024-06-03 23:59:23 -0400

    update src/VHS/VHS.Core/errorhandling/ErrorHandler.cs

--- src/VHS/VHS.Core/errorhandling/ErrorHandler.cs
+++ src/VHS/VHS.Core/errorhandling/ErrorHandler.cs
@@ -1,22 +1,15 @@
 using Serilog;
+using VHS.Core.Logging;
 using System;
 
 namespace VHS.Core.ErrorHandling
 {
     public static class ErrorHandler
     {
-        static ErrorHandler()
-        {
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-        }
-
         public static void HandleError(Exception ex)
         {
-            // Log the error with Serilog
-            Log.Error(ex, "An error occurred");
+            // Log the error using the shared logger
+            LoggingService.Logger.Error(ex, "An error occurred");
 
             // Additional actions can be added here
         }
@@ -24,7 +17,7 @@ namespace VHS.Core.ErrorHandling
         public static void HandleError(string customMessage, Exception ex)
         {
             // Log the custom error message along with exception details
-            Log.Error(ex, customMessage);
+            LoggingService.Logger.Error(ex, customMessage);
 
             // Additional actions can be added here
         }
